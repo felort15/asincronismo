10d8129aac71d92f238ec9968d310d29d6cdde91
Felipe Ortiz <felort15@gmail.com>
2024-09-15 13:40:47 -0600

    promise

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,20 +1,36 @@
-function sum(num1, num2) {
-    return num1 + num2;
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi) {
+  return new Promise((resolve, reject) => {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', urlApi, true);
+    xhttp.onreadystatechange = function (event) {
+      if (xhttp.readyState === 4) {
+        if (xhttp.status === 200) {
+          resolve(JSON.parse(xhttp.responseText));
+        } else {
+          reject(new Error(`Error ${xhttp.status}: ${xhttp.statusText}`));
+        }
+      }
+    }
+    xhttp.send();
+  });
 }
 
-function rest(num1, num2) {
-    return num1 - num2;
-}
-
-function mult(num1, num2) {
-    return num1 * num2;
-}
-
-function div(num1, num2) {
-    return num1 / num2;
-}
-
-function calc(num1, num2, callback) {
-    return callback(num1, num2);
-};
-console.log (calc (2,2, sum))
+fetchData(`${API}/products`)
+  .then(data1 => {
+    console.log(data1[0]);
+    return fetchData(`${API}/products/${data1[0].id}`);
+  })
+  .then(data2 => {
+    console.log(data2.title);
+    return fetchData(`${API}/categories/${data2?.category?.id}`);
+  })
+  .then(data3 => {
+    console.log(data3.name);
+  })
+  .catch(error => {
+    console.error(error);
+  });
